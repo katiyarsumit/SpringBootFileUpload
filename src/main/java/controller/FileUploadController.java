1fa8552ea55afb53d82fa0823d49176eddadc645
katiyarsumit <48628803+katiyarsumit@users.noreply.github.com>
2019-07-04 15:30:09 +0530

    Update FileUploadController.java

--- src/main/java/controller/FileUploadController.java
+++ src/main/java/controller/FileUploadController.java
@@ -1,28 +1,85 @@
-package controller;
+package com.example.demo.serviceImpl;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import com.example.demo.service.CompanyDomain;
 
-@Controller
-public class FileUploadController {
-  public static String uploadDirectory = System.getProperty("user.dir")+"/uploads";
-  @RequestMapping("/")
-  public String UploadPage(Model model) {
-	  return "uploadview";
-  }
-  @RequestMapping("/upload")
-  public String upload(Model model,@RequestParam("files") MultipartFile[] files) {
-	  StringBuilder fileNames = new StringBuilder();
-	  for (MultipartFile file : files) {
+@Service
+public class CompanyDomainImpl implements CompanyDomain {
+	String encoding = "UTF-8";
+	//public static String filePath = "E://spring boot learning/demo/src/main/resources/static/";
+	static String userPath=System.getProperty("user.dir");
+	static String replaceString=userPath.replace('\\','/');
+	static String replaceString1=replaceString.replaceFirst("/","//");
+	public static String filePath=replaceString1+"/src/main/resources/static/";
+	 public static String uploadDirectory = System.getProperty("user.dir")+"/src/main/resources/static";
+	
+	
+	public String convertFile(MultipartFile file) {
+		String downloadUrl="";
+		try {
+			Path fileName = convertMultiPartToFileAndSave1(file);
+			downloadUrl=fileName(file);
+			String domainName;
+			FileInputStream fis = new FileInputStream(new File(fileName.toString()));
+			@SuppressWarnings("resource")
+			XSSFWorkbook workbook = new XSSFWorkbook (fis);
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> ite = sheet.rowIterator();
+			while(ite.hasNext()){
+				Row row = (Row) ite.next();
+				Cell c = row.getCell(0);
+				String searchText =c.toString();
+				Document google = Jsoup.connect("https://www.google.com/search?q=" + URLEncoder.encode(searchText, encoding)).get();
+				Elements webSitesLinks = google.getElementsByTag("cite");					
+				if (webSitesLinks.isEmpty()) {
+					domainName="######## no result found for ##### ";
+					
+				}else {
+					domainName = webSitesLinks.get(0).text();
+				}
+				
+				Cell c1=row.createCell(1);
+				c1.setCellValue(domainName);
+				fis.close();
+				FileOutputStream fos =new FileOutputStream(new File(fileName.toString()));
+				workbook.write(fos);
+		        fos.close();
+			}
+			System.out.println("successfully executed");
+			System.out.println("uploadDirectory file path : "+uploadDirectory);
+			System.out.println("dynamic file path : "+fileName);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		return downloadUrl;
+	}
+	
+	static Path convertMultiPartToFileAndSave1(MultipartFile file) throws IOException {
+		StringBuilder fileNames = new StringBuilder();
+		 File directory = new File("uploads");
+		/* if(!directory.exists()) {
+			 directory.mkdir();
+			 System.out.println(directory);
+		 }*/
 		  Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
 		  fileNames.append(file.getOriginalFilename()+" ");
 		  try {
@@ -30,10 +87,19 @@ public class FileUploadController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	  }
-	  model.addAttribute("msg", "Successfully uploaded files "+fileNames.toString());
-	  return "uploadstatusview";
-  }
-  
-  
+		return fileNameAndPath;
+	}
+	
+	static String convertMultiPartToFileAndSave(MultipartFile file) throws IOException {
+		String fileName = file.getOriginalFilename();
+		FileOutputStream fos = new FileOutputStream(filePath + fileName);
+		fos.write(file.getBytes());
+		fos.close();
+		return fileName;
+	}
+	
+	static String fileName(MultipartFile file) {
+		return file.getOriginalFilename();
+	}
+	
 }
